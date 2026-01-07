2743114955033da584b0357794505a0dddf50c0b
Mario <kuatsimoto@gmail.com>
2026-01-07 02:25:56 -0600

    Fixed cycles in opcode table

--- src/cpu/opcode_lookup.rs
+++ src/cpu/opcode_lookup.rs
@@ -42,29 +42,33 @@ pub fn handler_dispatch(cpu: &mut CPU, instruction: &Instruction) {
     }
 }
 
+//NOTE: Cycles need to be either an enum or a hashmap. 
+//Need to detect cases where it can be higher if page crossed
+
 pub static OPCODE_LOOKUP: Lazy<HashMap<u8, Instruction>> = Lazy::new(|| {
     let mut m = HashMap::new();
     //LDA Opcodes
     m.insert(0xA9u8, Instruction{operation:Operation::LDA, addressing:AddressMode::Immediate, cycles:2});
-    m.insert(0xA5u8, Instruction{operation:Operation::LDA, addressing:AddressMode::ZeroPage, cycles:2});
-    m.insert(0xB5u8, Instruction{operation:Operation::LDA, addressing:AddressMode::ZeroPageIndexedX, cycles:2});
-    m.insert(0xADu8, Instruction{operation:Operation::LDA, addressing:AddressMode::Absolute, cycles:2});
-    m.insert(0xBDu8, Instruction{operation:Operation::LDA, addressing:AddressMode::AbsoluteIndexedX, cycles:2});
-    m.insert(0xB9u8, Instruction{operation:Operation::LDA, addressing:AddressMode::AbsoluteIndexedY, cycles:2});
-    m.insert(0xA1u8, Instruction{operation:Operation::LDA, addressing:AddressMode::IndexedIndirectX, cycles:2});
+    m.insert(0xA5u8, Instruction{operation:Operation::LDA, addressing:AddressMode::ZeroPage, cycles:3});
+    m.insert(0xB5u8, Instruction{operation:Operation::LDA, addressing:AddressMode::ZeroPageIndexedX, cycles:4});
+    m.insert(0xADu8, Instruction{operation:Operation::LDA, addressing:AddressMode::Absolute, cycles:4});
+    m.insert(0xBDu8, Instruction{operation:Operation::LDA, addressing:AddressMode::AbsoluteIndexedX, cycles:4});
+    m.insert(0xB9u8, Instruction{operation:Operation::LDA, addressing:AddressMode::AbsoluteIndexedY, cycles:4});
+    m.insert(0xA1u8, Instruction{operation:Operation::LDA, addressing:AddressMode::IndexedIndirectX, cycles:6});
+    m.insert(0xB1u8, Instruction{operation:Operation::LDA, addressing:AddressMode::IndexedIndirectY, cycles:5});
     
     //LDX Opcodes
-    m.insert(0xB1u8, Instruction{operation:Operation::LDX, addressing:AddressMode::Immediate, cycles:2});
-    m.insert(0xA2u8, Instruction{operation:Operation::LDX, addressing:AddressMode::ZeroPage, cycles:2});
-    m.insert(0xA6u8, Instruction{operation:Operation::LDX, addressing:AddressMode::ZeroPageIndexedY, cycles:2});
-    m.insert(0xAEu8, Instruction{operation:Operation::LDX, addressing:AddressMode::Absolute, cycles:2});
-    m.insert(0xBEu8, Instruction{operation:Operation::LDX, addressing:AddressMode::AbsoluteIndexedY, cycles:2});
+    m.insert(0xA2u8, Instruction{operation:Operation::LDX, addressing:AddressMode::Immediate, cycles:2});
+    m.insert(0xA6u8, Instruction{operation:Operation::LDX, addressing:AddressMode::ZeroPage, cycles:3});
+    m.insert(0xB6u8, Instruction{operation:Operation::LDX, addressing:AddressMode::ZeroPageIndexedY, cycles:4});
+    m.insert(0xAEu8, Instruction{operation:Operation::LDX, addressing:AddressMode::Absolute, cycles:4});
+    m.insert(0xBEu8, Instruction{operation:Operation::LDX, addressing:AddressMode::AbsoluteIndexedY, cycles:4});
 
     //LDY Opcodes
     m.insert(0xA0u8, Instruction{operation:Operation::LDY, addressing:AddressMode::Immediate, cycles:2});
-    m.insert(0xA4u8, Instruction{operation:Operation::LDY, addressing:AddressMode::ZeroPage, cycles:2});
-    m.insert(0xB4u8, Instruction{operation:Operation::LDY, addressing:AddressMode::ZeroPageIndexedY, cycles:2});
-    m.insert(0xACu8, Instruction{operation:Operation::LDY, addressing:AddressMode::Absolute, cycles:2});
-    m.insert(0xBCu8, Instruction{operation:Operation::LDY, addressing:AddressMode::AbsoluteIndexedY, cycles:2});
+    m.insert(0xA4u8, Instruction{operation:Operation::LDY, addressing:AddressMode::ZeroPage, cycles:3});
+    m.insert(0xB4u8, Instruction{operation:Operation::LDY, addressing:AddressMode::ZeroPageIndexedY, cycles:4});
+    m.insert(0xACu8, Instruction{operation:Operation::LDY, addressing:AddressMode::Absolute, cycles:4});
+    m.insert(0xBCu8, Instruction{operation:Operation::LDY, addressing:AddressMode::AbsoluteIndexedY, cycles:4});
     m
 });
